bb236974a43ac6bf0927ea0341a3f7c8c3829bab
Erebus1L <89197840+Erebus1L@users.noreply.github.com>
2023-11-17 21:07:57 +0530

    First Commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,6 +33,16 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: 'sepolia',
+    networks:{
+      hardhat: {
+        chainId: 11155111
+      },
+      sepolia: {
+        url: 'https://sepolia.rpc.thirdweb.com',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
